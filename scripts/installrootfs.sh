8b352dce9e8c78313cff0501c88d94c6bfdbe4e6
Peter Bouda <pbouda@cidles.eu>
2015-06-03 14:06:15 +0100

    Updated installrootfs script.

--- scripts/installrootfs.sh
+++ scripts/installrootfs.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 echo "Installing root fs..."
-umount /dev/sdc1
-umount /dev/sdc2
-sudo mkfs.ext4 -i 1024 -L rootfs /dev/sdc2
-sudo mount /dev/sdc2 /mnt
+umount /dev/sdX1
+umount /dev/sdX2
+sudo mkfs.ext4 -i 1024 -L rootfs /dev/sdX2
+sudo mount /dev/sdX2 /mnt
 cd /mnt
 sudo tar xvf $1/rootfs.tar
 cd $1
-sudo umount /dev/sdc2
+sudo umount /dev/sdX2
 
 echo "Installing kernel..."
-sudo mount /dev/sdc1 /mnt
-#../../../tools/mkimage/mkknlimg $1/zImage $1/zImage.dt
+sudo mount /dev/sdX1 /mnt
 sudo cp $1/zImage /mnt/kernel.img
 sudo cp $1/zImage /mnt/kernel7.img
-sudo umount /dev/sdc1
+sudo umount /dev/sdX1
